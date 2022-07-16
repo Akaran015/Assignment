7d1edefdb45ad35c5dc7237be4e51ecca6f5f62d
Mohit-Kumar98 <mohitkumar98.clg@gmail.com>
2022-07-16 14:21:02 +0530

    Done

--- Assignment 2/Question12.c
+++ Assignment 2/Question12.c
@@ -7,7 +7,7 @@ int main()
     float inr,usd=1.0;
     printf("Enter the amount in INR: ");
     scanf("%f",&inr);
-    usd=inr*76.23;
-    printf("Amount in USD is %.2f",usd);
+    usd=inr/76.23;
+    printf("Amount in USD is %.2f$",usd);
     return 0;
 }
--- Assignment 2/Question13.c
+++ Assignment 2/Question13.c
@@ -5,7 +5,7 @@ int main()
     int num;
     printf("Enter the Number: ");
     scanf("%d",&num);
-    num=num>>1;
+    num=num%10*100+num/10;
     printf("Number is %d",num);
     return 0;
 }
