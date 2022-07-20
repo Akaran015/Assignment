78fc87c3ad4bf8659fd8fa7cda9ca9bafb66a02d
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2022-07-20 19:41:33 +0530

    Add files via upload

---
+++ Assignment 8/Question5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int var;
+	printf("Enter var number(1-3): ");
+	scanf("%d", &var);
+
+	switch (var)
+	{
+	case 1:
+		printf("Good");
+		break;
+	case 2:
+		printf("Better");
+		break;
+	case 3:
+		printf("Best");
+		break;
+	default:
+		printf("Invalid");
+
+	}
+	return 0;
+}
