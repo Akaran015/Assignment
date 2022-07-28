68dbb9a2a5dc1962d221e7eb52012a222ac970a0
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2022-07-28 15:46:43 +0530

    update

--- Assignment 9/Question7.c
+++ Assignment 9/Question7.c
@@ -1,71 +1,37 @@
-#include<stdio.h>
-
-int
-main ()
+#include <stdio.h>
+int main()
 {
+    float unit, amount = 0, total = 0;
+    printf("Enter the number of units you consumed: ");
+    scanf("%f", &unit);
 
-	int unit;
+    switch (unit <= 50)
+    {
+    case 1:
+        amount = unit * 0.50;
+        break;
+    case 0:
+        switch (unit <= 150)
+        {
+        case 1:
+            amount = 25 + (unit - 50) * 0.75;
+            break;
+        case 0:
+            switch (unit <= 250)
+            {
+            case 1:
+                amount = 125 + (unit - 150) * 1.20;
+                break;
+            case 0:
+                amount = 245 + (unit - 250) * 1.50;
+                break;
+            }
+            break;
+        }
+        break;
+    }
 
-	float amount, total_amount, s_charge;
-
-	printf ("Enter the number of units you consumed: ");
-
-	scanf ("%d", &unit);
-
-	switch (unit <= 50)
-
-	{
-
-	case 1:
-
-		amount = unit * 0.50;
-
-		break;
-
-	case 0:
-
-		switch (unit <= 150)
-
-		{
-
-		case 1:
-
-			amount = 25 + (unit - 50) * 0.75;
-
-			break;
-
-		case 0:
-
-			switch (unit <= 250)
-
-			{
-
-			case 1:
-
-				amount = 100 + (unit - 150) * 1.20;
-
-				break;
-
-			case 0:
-
-				amount = 220 + (unit - 250) * 1.50;
-
-				break;
-
-			}
-
-			break;
-
-		}
-
-		break;
-
-	}
-
-	s_charge = amount * 0.20;
-
-	total_amount = amount + s_charge;
-
-	printf("your total bill is Rs %.2f", total_amount);
-
+    total = amount + amount * 0.20;
+    printf("your total bill is Rs %.2f ", total);
+    return 0;
 }
