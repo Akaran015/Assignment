010cde67b6227b389551adc905d43f362176c2c2
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2022-07-29 14:10:59 +0530

    Done

--- Assignment 11/Question8.c
+++ Assignment 11/Question8.c
@@ -5,6 +5,7 @@ int main()
     Pascal();
     return 0;
 }
+
 void Pascal()
 {
 
@@ -26,38 +27,3 @@ void Pascal()
         printf("\n");
     }
 }
-
-// int Comb(int, int);
-// int fact(int);
-
-// int main()
-// {
-//     int n, r, x;
-
-//     printf("Enter n items: ");
-//     scanf("%d", &n);
-
-//     printf("Enter r selected at a time: ");
-//     scanf("%d", &r);
-
-//     x = Comb(n, r);
-
-//     printf("N items and r selected at a time %d \n", x);
-//     return 0;
-// }
-
-// int fact(int x)
-// {
-//     int i,f=1;
-//     for (i = 1; i <= x; i++)
-//         f*=i;
-//         return f;
-// }
-
-// int Comb(int n, int r)
-// {
-//     int ans=0;
-//     ans=fact(n)/(fact(r)*fact(n-r));
-
-//     return ans;
-// }
